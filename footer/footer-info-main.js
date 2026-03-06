7693783688b0ee9e697b78a69b9f092f40132fd7
dirqenstudio23 <dirqen.studio@gmail.com>
2026-03-06 21:17:08 +0700

    w

--- footer/footer-info-main.js
+++ footer/footer-info-main.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
   fetch('https://dirqenstudio23.github.io/kitbashed/footer/footer-info-main.html')
     .then(res => res.text())
     .then(html => {
-      document.getElementById('footer-info').innerHTML = html;
+      document.getElementById('footer-info-main').innerHTML = html;
     })
     .catch(err => console.error('Gagal load navbar:', err));
 });
