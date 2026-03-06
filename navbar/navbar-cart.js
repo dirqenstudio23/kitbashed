290a38a239f36833677ef7783972dc6a45f458ff
dirqenstudio23 <dirqen.studio@gmail.com>
2026-03-06 21:47:47 +0700

    s

--- navbar/navbar-cart.js
+++ navbar/navbar-cart.js
@@ -1,10 +1,10 @@
 
 (function () {
 
-  const NAVBAR_PATH = 'https://dirqenstudio23.github.io/kitbashed/navbar/navbar.html';
+  const NAVBAR_PATH = 'https://dirqenstudio23.github.io/kitbashed/navbar/navbar-cart.html';
 
   function loadNavbar() {
-    const container = document.getElementById('navbar-container');
+    const container = document.getElementById('navbar-container-cart');
 
     if (!container) {
       console.warn('[Navbar Loader] Elemen <div id="navbar-container"> can not find in this page.');
@@ -14,7 +14,7 @@
     fetch(NAVBAR_PATH)
       .then(function (response) {
         if (!response.ok) {
-         throw new Error('Failed. Status: ' + response.status + '. ensure path "' + NAVBAR_PATH + '" correct.');
+          throw new Error('Failed. Status: ' + response.status + '. ensure path "' + NAVBAR_PATH + '" correct.');
         }
         return response.text();
       })
