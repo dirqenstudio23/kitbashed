d26895b00aae89a8cffb43e0162ba5defbe7ea03
dirqenstudio23 <dirqen.studio@gmail.com>
2026-04-10 23:04:02 +0700

    sd

---
+++ footer/CTA/cta-footer.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function () {
+  fetch("https://dirqenstudio23.github.io/kitbashed/footer/CTA/cta-footer.html")
+    .then((res) => res.text())
+    .then((html) => {
+      document.getElementById("footer-info").innerHTML = html;
+    })
+    .catch((err) => console.error("Gagal load navbar:", err));
+});
